dc7ddd6e2a6aecd11d9f6d9d6f67ef77e9464532
Linus Wallgren <linus.wallgren@gmail.com>
2017-07-07 22:02:22 +0200

    Create thumbs and scaledown images

--- src/fgallery.py
+++ src/fgallery.py
@@ -6,6 +6,8 @@ import sys
 from datetime import datetime
 from typing import Optional
 
+import subprocess
+
 import exifread
 import os
 import shutil
@@ -101,38 +103,130 @@ def get_dimensions(tags):
                          tags['EXIF ExifImageLength'].values[0])
 
 
+def process_image(image):
+    tags = get_exif(image)
+    taken_date = parse_taken_date(tags)
+    if not taken_date:
+        print('Failed to parse taken_date from %s' % image, file=sys.stderr)
+        return
+    size = get_dimensions(tags)
+    if not size:
+        print('Failed to parse size of %s' % image, file=sys.stderr)
+        return
+    relative_path = path.relpath(image, args.input)
+    relative_dir = path.dirname(relative_path)
+
+    create_folders(relative_dir)
+
+    scaledown_size = create_scaledown(image, relative_dir)
+    thumb_size = create_thumb(image, relative_dir)
+    create_original(image, relative_dir)
+
+    return create_datadto(relative_path, scaledown_size, thumb_size, size, taken_date)
+
+
+def create_original(image, relative_dir):
+    shutil.copy(image, path.join(args.output, 'files', relative_dir))
+
+
+class Image:
+    def __init__(self, image_path):
+        self.path = image_path
+        self.exif_tags = get_exif(self.path)
+
+    @property
+    def size(self):
+        return get_dimensions(self.exif_tags)
+
+
+def create_thumb(image, relative_dir):
+    thumb_quality = 90
+    min_thumb_size = Dimension(150.0, 112.0)  # Use floats for floating precision below
+    max_thumb_size = Dimension(267.0, 200.0)  # Use floats for floating precision below
+
+    destination = path.join(args.output, 'thumbs', relative_dir, path.basename(image))
+    size = get_dimensions(get_exif(image))
+
+    if size.x / size.y < min_thumb_size.x / min_thumb_size.y:
+        thumb_ratio = min_thumb_size.x / size.x
+    else:
+        thumb_ratio = min_thumb_size.y / size.y
+
+    sthumb = Dimension(max(round(size.x * thumb_ratio), min_thumb_size.x),
+                       max(round(size.y * thumb_ratio), min_thumb_size.y))
+
+    mthumb = Dimension(min(max_thumb_size.x, sthumb.x), min(max_thumb_size.y, sthumb.y))
+
+    # face/center detection
+    center = Dimension(0.5, 0.5)
+
+    def clamp(a, b, v):
+        return max(a, min(b, v))
+
+    # cropping window
+    dx = sthumb.x - mthumb.x
+    cx = clamp(0, dx, int(center.x * sthumb.x - sthumb.x / 2 + dx / 2))
+    dy = sthumb.y - mthumb.y
+    cy = clamp(0, dy, int(center.y * sthumb.y - sthumb.y / 2 + dy / 2))
+
+    cmd = ['convert',
+           '-quiet', image,
+           '-gamma', '0.454545',
+           '-resize', '%sx%s!' % (sthumb.x, sthumb.y),
+           '-gravity', 'NorthWest',
+           '-crop', '%sx%s+%s+%s' % (mthumb.x, mthumb.y, cx, cy),
+           '-gamma', '2.2',
+           '+profile', '!icc,*',
+           '-quality', str(thumb_quality),
+           destination
+           ]
+    execute(cmd)
+    return mthumb
+
+
+def execute(cmd):
+    print(cmd)
+    return subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
+
+
+def create_scaledown(image, relative_dir):
+    max_full_size = Dimension(1600, 1200)
+    image_quality = 90
+
+    destination = path.join(args.output, 'imgs', relative_dir, path.basename(image))
+
+    cmd = ['convert',
+           '-quiet',
+           image,
+           '-gamma', '0.454545',
+           '-geometry', '%sx%s>' % (max_full_size.x, max_full_size.y),
+           '-print', '%w %h',
+           '-gamma', '2.2',
+           '+profile', '!icc,*',
+           '-quality',
+           str(image_quality),
+           destination
+    ]
+    completed_process = execute(cmd)
+    (width_bytes, height_bytes) = completed_process.stdout.split(b' ')
+
+    return Dimension(int(width_bytes), int(height_bytes))
+
+
 def main():
-    for directory in ('imgs', 'thumbs', 'files'):
-        os.makedirs(path.join(args.output, directory), exist_ok=True)
-
-    datas = []
-
-    for image in images_to_process(args.input):
-        tags = get_exif(image)
-        taken_date = parse_taken_date(tags)
-        if not taken_date:
-            print('Failed to parse taken_date from %s' % image, file=sys.stderr)
-            continue
-        size = get_dimensions(tags)
-        if not size:
-            print('Failed to parse size of %s' % image, file=sys.stderr)
-            continue
-        relative_path = path.relpath(image, args.input)
-        relative_dir = path.dirname(relative_path)
-
-        for directory in ('imgs', 'thumbs', 'files'):
-            os.makedirs(path.join(args.output, directory, relative_dir), exist_ok=True)
-
-        shutil.copy(image, path.join(args.output, 'imgs', relative_dir))
-        shutil.copy(image, path.join(args.output, 'thumbs', relative_dir))
-        shutil.copy(image, path.join(args.output, 'files', relative_dir))
-        datas += [create_datadto(relative_path, size, size, size, taken_date)]
+    datas = [process_image(image) for image in images_to_process(args.input)]
+    datas = [data for data in datas if data]
 
     with open(path.join(args.output, 'data.json'), mode='w', encoding='utf-8') as f:
         f.write(json.dumps(create_rootdto(images=datas,
                                           thumb=create_thumbdto(Dimension(100, 100),
                                                                 Dimension(200, 200)),
                                           blur=Dimension(100, 200))))
+
+
+def create_folders(target_dir):
+    for directory in ('imgs', 'thumbs', 'files'):
+        os.makedirs(path.join(args.output, directory, target_dir), exist_ok=True)
 
 
 main()
